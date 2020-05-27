16a8a9a21f085ef30c6eee8cbe6e43e14f0d1423
Tony Huang <thuang12@hotmail.com>
2020-05-26 23:59:06 -0700

    Updated utils library

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,4 @@
-def get_paginated_questions(request, questions, num_of_questions):
+def get_paginated_questions(request, questions, num_of_questions=10):
     page = request.args.get('page', 1, type=int)
     start = (page - 1) * num_of_questions
     end = start + num_of_questions
