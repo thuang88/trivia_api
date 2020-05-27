8c4bc8c7518b0556c3f3dd7ccf3b9d6abab89eea
Tony Huang <thuang12@hotmail.com>
2020-05-27 00:00:19 -0700

    added more unit tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -42,7 +42,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data["categories"]))
 
     def test_get_paginated_questions(self):
-        res = self.client().get('/questions?page=1')
+        res = self.client().get('/questions')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -63,8 +63,8 @@ class TriviaTestCase(unittest.TestCase):
 
     def test_create_question(self):
         question_data = {
-            'question': 'a',
-            'answer': 'a',
+            'question': 'this is a test',
+            'answer': 'this is a test',
             'difficulty': 1,
             'category': 1
         }
@@ -75,12 +75,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         self.assertEqual(data["success"], True)
 
-    #def test_delete_question(self):
-    #    res = self.client().delete('/questions/23')
-    #    data = json.loads(res.data)
+    def test_delete_question(self):
+        question = Question(
+            question='this is another test',
+            answer='this is another test',
+            difficulty=1,
+            category=1
+        )
+        question.insert()
 
-    #    self.assertEqual(res.status_code, 200)
-    #    self.assertTrue(data["success"])
+        res = self.client().delete('/questions/{}'.format(question.id))
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data["success"])
 
     def test_search_questions(self):
         search_data = {'searchTerm': 'dung beetle'}
@@ -91,14 +99,29 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertTrue(len(data['questions']))
 
-    #def test_get_questions_by_category(self):
-    #    res = self.client().get('/categories/6/questions')
-    #    data = json.loads(res.data)
+    def test_get_questions_by_category(self):
+        res = self.client().get('/categories/6/questions')
+        data = json.loads(res.data)
 
-        #self.assertEqual(res.status_code, 200)
-        #self.assertTrue(data['success'])
-        #self.assertNotEqual(len(data['questions']), 0)
-        #self.assertEqual(data['category'], 'Sports')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertNotEqual(len(data['questions']), 0)
+        self.assertEqual(data['category'], 'Sports')
+
+    def test_get_quiz_question(self):
+        quiz_data = {
+            'previous_questions': [16, 17],
+            'quiz_category': {
+                'id': 2,
+                'type': 'Art'
+            }
+        }
+
+        res = self.client().post('/quizzes', json=quiz_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
