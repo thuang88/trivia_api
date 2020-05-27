83470d91ca624f9ffd79b36ccd0f57f7ba364399
Tony Huang <thuang12@hotmail.com>
2020-05-26 18:50:29 -0700

    add utils library

---
+++ backend/utils.py
@@ -0,0 +1,9 @@
+def get_paginated_questions(request, questions, num_of_questions):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * num_of_questions
+    end = start + num_of_questions
+
+    questions = [question.format() for question in questions]
+    current_questions = questions[start:end]
+
+    return current_questions
